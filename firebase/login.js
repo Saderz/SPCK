3a3a7dd53a2688b2c133e275811cf603012af028
Sader <khieuhoangminhpro@gmail.com>
2023-09-17 19:37:08 +0700

    update 1

---
+++ firebase/login.js
@@ -0,0 +1,27 @@
+// import { auth, db } from "./config.js";
+// import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js"
+// import { collection, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js"
+
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const registerBtn = document.getElementById("register-btn");
+// const errorMessage = document.getElementById("error-message");
+
+// const handleRegister = () => {
+//     signInWithEmailAndPassword(auth, email.value, password.value)
+//     .then((userCredential) => {
+//         const user = userCredential.user;
+//         console.log(user.email);
+//     })
+//     .catch((error) => {
+//         if (error.code === "auth/invalid-email") {
+//             errorMessage.textContent = "Email không hợp lệ";
+//         } else if (error.code === "auth/missing-password") {
+//             errorMessage.textContent = "Chưa nhập password";
+//         }
+//     });
+// };
+
+// const saveUser = async (email) => {}
+
+// loginBtn.addEventListener("click", handleRegister);
