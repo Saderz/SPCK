7c425c684b342b2ccebb40e2e9cea3a405e2d6ef
Sader <khieuhoangminhpro@gmail.com>
2023-10-06 17:10:32 +0700

    update code 4

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ const whyUs = document.getElementById("why-us");
 const whyUsBTN = document.getElementById("why-us-btn");
 
 whyUsBTN.addEventListener('click', () => {
-    whyUs.scrollIntoView({
-        behavior: "smooth"
-    })
-})
+    // whyUs.scrollIntoView({
+    //     behavior: "smooth"
+    // })
+    // console.log("code bth");
+    whyUs.scrollIntoView();
+    whyUs.style.scrollBehavior = "smooth";
+});
