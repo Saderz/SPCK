eec3666641537eef7888df3b5da770abe928fdc4
minhquang55 <minhquangam@gmail.com>
2023-10-07 11:14:25 +0700

    Change display username instead of email

--- firebase/register.js
+++ firebase/register.js
@@ -1,25 +1,41 @@
 import { auth, db } from "./config.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js"
-import { collection, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js"
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+import {
+  collection,
+  doc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
 
 const email = document.getElementById("register-email");
 const password = document.getElementById("register-password");
 const registerBtn = document.getElementById("formRegister");
+const username = document.getElementById("username");
 const errorMessage = document.getElementById("error-message");
 
 const handleRegister = (e) => {
-    e.preventDefault();
-    createUserWithEmailAndPassword(auth, email.value, password.value)
+  e.preventDefault();
+  createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((userCredential) => {
-        const user = userCredential.user;
-        window.location = "./login.html";
+      const user = userCredential.user;
+      updateProfile(auth.currentUser, {
+        displayName: username.value,
+      })
+        .then(() => {
+          window.location = "./login.html";
+        })
+        .catch((error) => {
+          alert(error.code);
+        });
     })
     .catch((error) => {
-        if (error.code === "auth/invalid-email") {
-            errorMessage.textContent = "Email không hợp lệ";
-        } else if (error.code === "auth/missing-password") {
-            errorMessage.textContent = "Chưa nhập password";
-        }
+      if (error.code === "auth/invalid-email") {
+        errorMessage.textContent = "Email không hợp lệ";
+      } else if (error.code === "auth/missing-password") {
+        errorMessage.textContent = "Chưa nhập password";
+      }
     });
 };
 
